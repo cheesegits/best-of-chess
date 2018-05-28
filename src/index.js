c58406b7c60110ff1e6cb5b4b174ba3c2be778f7
cheesegits <mattmulit@hotmail.com>
2018-05-28 17:59:42 -0400

    Connected React to Apollo Client

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
+
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+
+const client = new ApolloClient();
+
+const App = () => (
+  <ApolloProvider client={client}>
+    <div>
+      <h2>React is now connected to the Apollo client...</h2>
+    </div>
+  </ApolloProvider>
+);
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
